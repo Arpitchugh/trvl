f8816555bacafd0bffe5e6a5843d6036341bf699
Arpit <arpitchugh.jbd.ac@gmail.com>
2021-07-28 20:25:50 +0530

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
-export default class Navbar extends Component {
-	render() {
-    return <>
-      <h1>hello</h1>
-    </>;
-	}
+function Navbar() {
+	// handles hambrger icon click motion
+	const [click, setClick] = useState(false);
+	const handleClick = () => {
+		setClick(!click);
+	};
+	const closeMobileMenu = () => setClick(false);
+
+	//handle btn
+	const [button, setButton] = useState(true);
+	const showButton = () => {
+		if (window.innerWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
+
+	useEffect(() => {
+		showButton();
+	}, []);
+
+	window.addEventListener('resize', showButton);
+
+	return (
+		<>
+			<nav className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-logo">{closeMobileMenu}
+						TRVL <i className="fab fa-typo3"></i>
+					</Link>
+					<div className="menu-icon" onClick={handleClick}>
+						<i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+					</div>
+					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+						<li className="nav-item">
+							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link
+								to="/Services"
+								className="nav-links"
+								onClick={closeMobileMenu}
+							>
+								Services
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link
+								to="/Products"
+								className="nav-links"
+								onClick={closeMobileMenu}
+							>
+								Products
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link
+								to="/sign-up"
+								className="nav-links-mobile"
+								onClick={closeMobileMenu}
+							>
+								Sign Up
+							</Link>
+						</li>
+					</ul>
+					{button && <Button buttonStyle="btn--outline">Sign Up</Button>}
+				</div>
+			</nav>
+		</>
+	);
 }
+
+export default Navbar;
